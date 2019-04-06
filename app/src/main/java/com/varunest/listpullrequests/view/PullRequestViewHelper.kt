cc55336ca8a09d378f167306a6c04f06b0e3da96
varunest <mailvarunest@gmail.com>
2019-04-06 22:13:10 +0530

    Enable experimental androidExtensions

--- app/src/main/java/com/varunest/listpullrequests/view/PullRequestViewHelper.kt
+++ app/src/main/java/com/varunest/listpullrequests/view/PullRequestViewHelper.kt
@@ -1,11 +1,12 @@
 package com.varunest.listpullrequests.view
 
 import android.view.View
+import kotlinx.android.extensions.LayoutContainer
 
 interface PullRequestViewHelper {
 
 }
 
-class PullRequestViewHelperImpl(val rootView: View) : PullRequestViewHelper {
-
+class PullRequestViewHelperImpl(val rootView: View) : PullRequestViewHelper, LayoutContainer {
+    override val containerView: View = rootView
 }
