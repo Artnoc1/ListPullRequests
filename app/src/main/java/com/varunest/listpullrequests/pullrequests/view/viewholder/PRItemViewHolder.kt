7066df63b38557f74752f59f4c9fdd2d7d47e506
varunest <mailvarunest@gmail.com>
2019-04-07 21:01:23 +0530

    Show time ago when pull request was opened

--- app/src/main/java/com/varunest/listpullrequests/pullrequests/view/viewholder/PRItemViewHolder.kt
+++ app/src/main/java/com/varunest/listpullrequests/pullrequests/view/viewholder/PRItemViewHolder.kt
@@ -1,21 +1,35 @@
 package com.varunest.listpullrequests.pullrequests.view.viewholder
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
+import com.varunest.listpullrequests.R
 import com.varunest.listpullrequests.data.network.model.PullRequest
+import com.varunest.listpullrequests.utils.CommonUtils
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_pr.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PRItemViewHolder(
-    val itemView: View,
-    override val containerView: View = itemView
-) : RecyclerView.ViewHolder(itemView), LayoutContainer {
-    
+    val view: View,
+    override val containerView: View = view
+) : RecyclerView.ViewHolder(view), LayoutContainer {
 
     fun bind(pullRequest: PullRequest?) {
         name.text = pullRequest?.user?.name
         title.text = pullRequest?.title
         body.text = pullRequest?.body
+        try {
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
+            simpleDateFormat.timeZone = TimeZone.getTimeZone("GMT")
+            val date = simpleDateFormat.parse(pullRequest?.createdAt)
+            val timeAgo = CommonUtils.getTimeAgo(date)
+            timeAgoTextView.text = view.context.getString(R.string.time_ago_string, timeAgo)
+        } catch (e: Exception) {
+            Log.e("PRItemViewHolder", "Failed to parse Date")
+        }
+
     }
 
 }
--- app/src/main/java/com/varunest/listpullrequests/utils/CommonUtils.kt
+++ app/src/main/java/com/varunest/listpullrequests/utils/CommonUtils.kt
@@ -9,11 +9,15 @@ import android.app.Activity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.support.v4.content.ContextCompat.getSystemService
-
-
+import android.text.format.DateUtils
+import java.util.*
 
 
 object CommonUtils {
+    private const val SECOND_MILLIS = 1000
+    private const val MINUTE_MILLIS = 60 * SECOND_MILLIS
+    private const val HOUR_MILLIS = 60 * MINUTE_MILLIS
+    private const val DAY_MILLIS = 24 * HOUR_MILLIS
 
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -25,4 +29,33 @@ object CommonUtils {
         val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
+
+    private fun currentDateUTC(): Date {
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        return calendar.time
+    }
+
+    fun getTimeAgo(date: Date): String {
+        var time = date.time
+        if (time < 1000000000000L) {
+            time *= 1000
+        }
+
+        val now = System.currentTimeMillis()
+        if (time > now || time <= 0) {
+            return "in the future"
+        }
+
+        val diff = now - time
+        return when {
+            diff < MINUTE_MILLIS -> "moments ago"
+            diff < 2 * MINUTE_MILLIS -> "a minute ago"
+            diff < 60 * MINUTE_MILLIS -> "${diff / MINUTE_MILLIS} minutes ago"
+            diff < 2 * HOUR_MILLIS -> "an hour ago"
+            diff < 24 * HOUR_MILLIS -> "${diff / HOUR_MILLIS} hours ago"
+            diff < 48 * HOUR_MILLIS -> "yesterday"
+            else -> "${diff / DAY_MILLIS} days ago"
+        }
+    }
+
 }
